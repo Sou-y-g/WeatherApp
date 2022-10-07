68a065f300dddfa3bb9d0fc6f6e02ba39b70fac6
Sou Yonamine <souyonamine@SounoMacBook-Pro.local>
2022-10-07 20:52:21 +0900

    del API

--- main2.js
+++
@@ -1,8 +0,0 @@
-const places = {
-    'pref': [34.0774397,134.5518621],
-    'katsura': [33.916884,134.472888],
-    'kamikatsu': [33.909891,134.369191],
-    'narutaki': [34.0774397,134.5518621],
-}
-
-console.log(places.katsura[0])
