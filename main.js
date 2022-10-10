8c219e833afc74e9645a146065ffa852f85a835f
Sou Yonamine <souyonamine@SounoMacBook-Pro.local>
2022-10-10 13:58:00 +0900

    first commit

--- main.js
+++ main.js
@@ -26,46 +26,64 @@ const tranceLang = {
   Snow: '雪',
 };
 
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+const URL5 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
 //URLを取得する処理
 function getURL() {
   //現在選択している場所を取得
   const selectPlace = document.getElementById('place').value;
-  console.log(selectPlace);
   const lat = places[selectPlace][0];
   const lon = places[selectPlace][1];
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
   // 天気情報の取得
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  const URL5 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  return [URL, URL5];
+  return url;
 }
 
 //URLからjsonを取得してdataに格納
-const getWeather = async () => {
-  const res = await fetch(getURL());
+const getWeather = async (url) => {
+  const res = await fetch(url);
+  // const res2 = await fetch(getURL());
   const data = await res.json();
+  // const data2 = await res2.json();
   return data;
 };
 
 //ページが読み込まれた時の処理
 addEventListener('load', async () => {
-  const data = await getWeather();
+  const data = await getWeather(URL);
+  // const data2 = await getWeather(URL5);
+  createDom(data);
   // JSONから天気を取得してHTMLの今日の天気に記載(ついでに日本語変換)
-  document.getElementById('weather').innerHTML = tranceLang[data.weather[0].main];
-  // 天気の画像表示、HTMLのimg要素取得
-  const image = document.getElementById('weatherimg');
-  // 要素のsrcに画像パス追加
-  const weatherInfo = data.weather[0].main;
-  image.src = `./img/${weatherInfo}.jpg`;
+  // document.getElementById('weather').innerHTML = tranceLang[data.weather[0].main];
+  // // 天気の画像表示、HTMLのimg要素取得
+  // const image = document.getElementById('weatherimg');
+  // // 要素のsrcに画像パス追加
+  // const weatherInfo = data.weather[0].main;
+  // image.src = `./img/${weatherInfo}.jpg`;
 });
 
 //プルダウンで他の地域を選択した時の処理(上と一緒)
 //上と同じ処理なので簡略化できないかな？
 const place = document.getElementById('place');
 place.addEventListener('change', async () => {
+  getURL();
   const data = await getWeather();
+  createDom(data);
+  // document.getElementById('weather').innerHTML = tranceLang[data.weather[0].main];
+  // //5日間の予報取得処理
+  // // document.getElementById('weather_5day').innerHTML = tranceLang[data.];
+  // const image = document.getElementById('weatherimg');
+  // const weatherInfo = data.weather[0].main;
+  // image.src = `./img/${weatherInfo}.jpg`;
+});
+
+const createDom = (data) => {
   document.getElementById('weather').innerHTML = tranceLang[data.weather[0].main];
+  //5日間の予報取得処理
+  // document.getElementById('weather_5day').innerHTML = tranceLang[data.];
   const image = document.getElementById('weatherimg');
   const weatherInfo = data.weather[0].main;
   image.src = `./img/${weatherInfo}.jpg`;
-});
+};
